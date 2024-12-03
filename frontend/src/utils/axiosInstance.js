ce23d4dff3b2115382b6d917eb573f69cac10c5e
piyush5090 <govindanipiyush10@gmail.com>
2024-12-04 02:14:31 +0530

    Deployed

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://vectorjobsapi-uce2gnnn.b4a.run/",
+  //baseURL: "https://vectorjobsapi-uce2gnnn.b4a.run/",
+  baseURL: "https://vectorjobs.onrender.com/",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
